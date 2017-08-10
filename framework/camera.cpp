386d3fe06c3ac98222c153e81fdf0e41a3e511f2
ConNoa <constantin.noack@uni-weimar.de>
2017-08-10 23:11:23 +0200

    camera updated

---
+++ framework/camera.cpp
@@ -0,0 +1,25 @@
+#include "camera.hpp"
+
+
+
+
+    Camera::Camera():
+        name_{"default"},
+        position_{0.0, 0.0, 0.0},
+        view_{0.0, 0.0, -1.0},
+        fov_x_{45.0}
+        {}
+
+    Camera::Camera(std::string const& name, glm::vec3 const& position, float fov_x):
+        name_{name},
+        position_{position},
+        view_{position},//muss nóch für die z achse einen negativeren wert bekommen als position
+        fov_x_{fov_x}
+        {}
+
+    Camera::Camera(std::string const& name, glm::vec3 const& position, glm::vec3 const& view, float fov_x):
+        name_{name},
+        position_{position},
+        view_{view},
+        fov_x_{fov_x}
+        {}
