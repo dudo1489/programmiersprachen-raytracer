f68a4db2921d633163c6ac8aa6a1b31c4663e401
ConNoa <constantin.noack@uni-weimar.de>
2017-08-13 16:57:55 +0200

    renderer scene included

--- framework/renderer.cpp
+++ framework/renderer.cpp
@@ -17,6 +17,15 @@ Renderer::Renderer(unsigned w, unsigned h, std::string const& file)
   , ppm_(width_, height_)
 {}
 
+Renderer::Renderer(unsigned w, unsigned h, std::string const& file, Scene const& scene)
+  : width_(w)
+  , height_(h)
+  , colorbuffer_(w*h, Color(0.0, 0.0, 0.0))
+  , filename_(file)
+  , ppm_(width_, height_)
+  , scene_(scene)
+{}
+
 void Renderer::render()
 {
   //const std::size_t checkersize = 20;
--- framework/renderer.hpp
+++ framework/renderer.hpp
@@ -20,6 +20,7 @@ class Renderer
 {
 public:
   Renderer(unsigned w, unsigned h, std::string const& file);
+  Renderer(unsigned w, unsigned h, std::string const& file Scene const& scene);
 
   void render();
   void write(Pixel const& p);
@@ -35,6 +36,7 @@ private:
   std::vector<Color> colorbuffer_;
   std::string filename_;
   PpmWriter ppm_;
+  Scene scene_;
 };
 
 #endif // #ifndef BUW_RENDERER_HPP
